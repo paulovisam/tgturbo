5a9bbaca59f4882cf8cae54cb0c48365c9dbb96b
Paulo Mendonça <paulo.mendonca@visie.com.br>
2025-03-21 10:04:43 -0300

    Atualiza a função get_chat_history para incluir um parâmetro from_msg_id e ajusta a tipagem do cliente

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import os
+from pyrogram.client import Client
 
 
 def create_path(path: str):
@@ -6,12 +7,11 @@ def create_path(path: str):
         os.makedirs(path)
     return path
 
-async def get_chat_history(client, origin_chat_id, last_msg_id):
+async def get_chat_history(client: Client, origin_chat_id: int | str, from_msg_id: int | str) -> list:
     messages = []
-    async for message in client.get_chat_history(
-            origin_chat_id,
-            offset_id=last_msg_id,
-    ):
+    async for message in client.get_chat_history(origin_chat_id):
+        if message.id <= from_msg_id:
+            break
         messages.append(message)
     messages.reverse()
-    return messages 
+    return messages
